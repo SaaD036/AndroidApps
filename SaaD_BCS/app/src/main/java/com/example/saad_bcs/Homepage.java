b7e03e77900d1d115e194ed80ab56a705c64cad3
SaaD_04 <shaad036@gmail.com>
2021-03-24 01:41:20 +0600

    Update: Creating the study helper application.

---
+++ SaaD_BCS/app/src/main/java/com/example/saad_bcs/Homepage.java
@@ -0,0 +1,21 @@
+package com.example.saad_bcs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        
+    }
+}
